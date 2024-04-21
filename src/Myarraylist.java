582fe69281801ec84e3e601bc1aa5c77d8231353
Engea <]zekenov05@list.ru>
2024-04-21 15:45:51 +0500

    2

--- src/Myarraylist.java
+++ src/Myarraylist.java
@@ -1,59 +1,91 @@
 import java.util.Iterator;
+import java.util.Objects;
 
-public class Myarraylist implements Mylist{
+public class Myarraylist<T> implements Mylist<T>{
+    private Object[] elements;
+    private int size;
+    private void ensureCapacity(int minCapacity) {
+        int oldCapacity = elements.length;
+        if (minCapacity > oldCapacity) {
+            int newCapacity = oldCapacity * 2;
+            if (newCapacity < minCapacity)
+                newCapacity = minCapacity;
+            Object[] newElements = new Object[newCapacity];
+            System.arraycopy(elements, 0, newElements, 0, size);
+            elements = newElements;
+        }
+    }
     @Override
     public void add(Object item) {
-
+        ensureCapacity(size + 1);
+        elements[size++] = item;
     }
 
     @Override
     public void set(int index, Object item) {
-
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+        elements[index] = item;
     }
 
     @Override
     public void add(int index, Object item) {
-
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException();
+        ensureCapacity(size + 1);
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = item;
+        size++;
     }
 
     @Override
     public void addFirst(Object item) {
-
+        add(0, item);
     }
 
     @Override
     public void addLast(Object item) {
+        add(size, item);
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+        return (T) elements[index];
 
     }
 
     @Override
-    public Object get(int index) {
-        return null;
+    public T getFirst() {
+        if (size == 0)
+            throw new IndexOutOfBoundsException();
+        return (T) elements[0];
     }
 
     @Override
-    public Object getFirst() {
-        return null;
-    }
-
-    @Override
-    public Object getLast() {
-        return null;
+    public T getLast() {
+        if (size == 0)
+            throw new IndexOutOfBoundsException();
+        return (T) elements[size - 1];
     }
 
     @Override
     public void remove(int index) {
-
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        elements[--size] = null;
     }
 
     @Override
     public void removeFirst() {
-
+        remove(0);
     }
 
     @Override
     public void removeLast() {
-
+        remove(size - 1);
     }
 
     @Override
@@ -63,32 +95,45 @@ public class Myarraylist implements Mylist{
 
     @Override
     public int indexOf(Object object) {
-        return 0;
+        for (int i = 0; i < size; i++) {
+            if (object.equals(elements[i]))
+                return i;
+        }
+        return -1;
     }
 
     @Override
     public int LastIndexOf(Object object) {
-        return 0;
+        for (int i = size - 1; i >= 0; i--) {
+            if (object.equals(elements[i]))
+                return i;
+        }
+        return -1;
     }
 
     @Override
     public boolean exists(Object object) {
-        return false;
+        return indexOf(object) != -1;
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] copy = new Object[size];
+        System.arraycopy(elements, 0, copy, 0, size);
+        return copy;
     }
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
--- src/Mylinkedlist.java
+++ src/Mylinkedlist.java
@@ -1,59 +1,132 @@
 import java.util.Iterator;
 
-public class Mylinkedlist implements Mylist{
-    @Override
-    public void add(Object item) {
+public class Mylinkedlist<T> implements Mylist<T>{
+    private class Node {
+        T data;
+        Node next;
+        Node prev;
 
+        Node(T data) {
+            this.data = data;
+        }
+    }
+    private Node head;
+    private Node tail;
+    private int size;
+    public Mylinkedlist() {
+        head = null;
+        tail = null;
+        size = 0;
     }
 
     @Override
-    public void set(int index, Object item) {
-
+    public void add(T item) {
+        addLast(item);
     }
 
     @Override
-    public void add(int index, Object item) {
-
+    public void set(int index, T item) {
+        getNode(index).data = item;
     }
 
     @Override
-    public void addFirst(Object item) {
-
+    public void add(int index, T item) {
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException();
+        if (index == 0)
+            addFirst(item);
+        else if (index == size)
+            addLast(item);
+        else {
+            Node newNode = new Node((T) item);
+            Node current = getNode(index);
+            Node prev = current.prev;
+            newNode.next = current;
+            newNode.prev = prev;
+            prev.next = newNode;
+            current.prev = newNode;
+            size++;
+        }
     }
 
     @Override
-    public void addLast(Object item) {
-
+    public void addFirst(T item) {
+        Node newNode = new Node(item);
+        if (size == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
     }
 
     @Override
-    public Object get(int index) {
-        return null;
+    public void addLast(T item) {
+        Node newNode = new Node(item);
+        if (size == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
     }
 
     @Override
-    public Object getFirst() {
-        return null;
+    public T get(int index) {
+        return getNode(index).data;
     }
 
     @Override
-    public Object getLast() {
-        return null;
+    public T getFirst() {
+        if (size == 0)
+            throw new IndexOutOfBoundsException();
+        return head.data;
+    }
+
+    @Override
+    public T getLast() {
+        if (size == 0)
+            throw new IndexOutOfBoundsException();
+        return tail.data;
     }
 
     @Override
     public void remove(int index) {
-
+        Node nodeToRemove = getNode(index);
+        Node prev = nodeToRemove.prev;
+        Node next = nodeToRemove.next;
+        if (prev != null)
+            prev.next = next;
+        else
+            head = next;
+        if (next != null)
+            next.prev = prev;
+        else
+            tail = prev;
+        nodeToRemove.data = null;
+        nodeToRemove.next = null;
+        nodeToRemove.prev = null;
+        size--;
     }
 
     @Override
     public void removeFirst() {
-
+        if (size == 0)
+            throw new IndexOutOfBoundsException();
+        remove(0);
     }
 
     @Override
     public void removeLast() {
-
+        if (size == 0)
+            throw new IndexOutOfBoundsException();
+        remove(size - 1);
     }
 
     @Override
@@ -63,36 +136,70 @@ public class Mylinkedlist implements Mylist{
 
     @Override
     public int indexOf(Object object) {
-        return 0;
+        int index = 0;
+        for (Node current = head; current != null; current = current.next) {
+            if (object.equals(current.data))
+                return index;
+            index++;
+        }
+        return -1;
     }
 
     @Override
     public int LastIndexOf(Object object) {
-        return 0;
+        int index = size - 1;
+        for (Node current = tail; current != null; current = current.prev) {
+            if (object.equals(current.data))
+                return index;
+            index--;
+        }
+        return -1;
     }
 
     @Override
     public boolean exists(Object object) {
-        return false;
+        return indexOf(object) != -1;
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] array = new Object[size];
+        int index = 0;
+        for (Node current = head; current != null; current = current.next) {
+            array[index++] = current.data;
+        }
+        return array;
     }
 
     @Override
     public void clear() {
-
+        while (head != null) {
+            Node next = head.next;
+            head.data = null;
+            head.next = null;
+            head.prev = null;
+            head = next;
+        }
+        tail = null;
+        size = 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public Iterator iterator() {
         return null;
+    }
+    private Node getNode(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current;
     }
 }
